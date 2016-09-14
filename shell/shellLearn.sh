2e3efde6623ca810e857c80c89d1229abd60779f
jeason <johnsonli_drum@163.com>
2016-09-14 18:02:25 +0800

    modified:   shell/shellLearn.sh

--- shell/shellLearn.sh
+++ shell/shellLearn.sh
@@ -3,23 +3,12 @@
 :<<!
 变量：字母开头，不能有空格，可以使用_，不能使用标点符号，不能使用bash里面的关键字
     赋值表达式是等号两边不能有空格
-!
-#只读变量
-func_1(){
-    var="aaa"
-    readonly var
-    var="bbb"    
-}
 
-#删除变量
-func_2(){
-    var="aaa"
-    echo $var
-    unset var
-    echo $var    
-}
+    定义变量  xxx=
+    输出。。  echo ${xxx}
+    只读变量  readonly xxx
+    删除变量  unset xxx
 
-:<<!
 字符串:
     单引号：内容原样输出
     双引号：可以有变量，可以有转义字符
@@ -28,7 +17,7 @@ func_2(){
 
 func_3(){
     var1="aaa"
-    str1="xxx,"$var1""
+    #str1="xxx,"$var1""
     str2="xxx,${var1}"
     echo $str1 $str2 
     echo ${#str2}         #获取字符串长度        
@@ -43,10 +32,11 @@ func_3(){
           array[0] = v1  ..
 
     读取：${array[0]}
+          ${array[@/*]}  读取全部
+          ${#array[@/*]} 数组长度
+          ${#array[0]}   单个元素长度
+          
 !
-func_4(){
-    arr1=()    
-}
 
 
 
