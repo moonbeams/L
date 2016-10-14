28f0726b96367b35ed942a4c052e66f22d9c5728
jeason <johnsonli_drum@163.com>
2016-10-14 19:09:51 +0800

    modified:   C/a.out 	new file:   C/myc-support.c 	modified:   C/myc.c

---
+++ C/myc-support.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+/* printf */
+void myprint(struct ){
+        
+}
--- C/myc.c
+++ C/myc.c
@@ -37,15 +37,83 @@ void para_test(void){
 }
 
 /* 布尔运算: && || !  */
-void boole_test(int x,int y,int z){
-    if (x<3 && y>3)
-        printf("ok\n");
-    else if (x)
+
+/* case 后必须跟常量表达式，同全局表量，编译时需要计算出值 */
+void switch_test(int day){
+    switch(day){
+    case 1:
+        printf("Monday\n");
+        break;
+    case 2:
+        printf("Tuesday\n");    
+        break;
+    }    
+}
+
+
+void print_logrithm(double x){
+    if (x <= 0.0){
+        printf("need positive number!\n");
+        return;
+    }    
+    printf("The log of x is %f",log(x));
+}
+
+
+/* int函数没有返回值   编译时加-Wall参数可以检测到 */
+void is_leap_year(int year){
+    if ((!(year % 4) && (year % 100)) || !(year % 400)) 
+        printf("%d is leap year\n",year);
+    else
+        printf("%d is not leap year\n",year);
+}
+
+
+/* 递归-阶乘  注意定义base case */
+int factorial(int n){
+    if (n == 0)
+        return 1;
+    else{
+        int recurse = factorial(n-1);
+        int result = n * recurse;
+        return result;
+    }    
+}
+
+void xx(int a,int b){
+    if (a % b == 0)
+        printf("%d",b);
+    else
+        xx(b,a%b);
+}
+
+
+int factorial_while(int n){
+    int result = 1;
+    while (n>0){
+        result *= n;
+        n = n-1;
+    }    
+    return result;
+}
+
+int factorial_dowhile(int n){
+    int result = 1;
+    do{
+        result *= n;
+        n = n-1;  
+    }while(n>0);
+    return result;
+}
+
+int factorial_for(int n){
+    return ;
 }
 
 /*局部变量可以用任意类型相符的表达式来初始化，而全局变量只能用常量表达式初始化*/
 
 int main(void){
-    para_test();
+    int result = factorial_dowhile(5);
+    printf("%d",result);
     return 0;
 }
